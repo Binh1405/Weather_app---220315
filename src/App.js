f55488704b0ebd1ee6259edcf22f851346ecbb83
binh1405 <binhhoangngoc@gmail.com>
2022-03-17 16:09:50 +0700

    render input box

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const api = {
 
 const App = () => {
   const [searchInput, setSearchInput] = useState("");
-  const [searchCity, setSearchCity] = useState("");
+  const [searchCity, setSearchCity] = useState("Paris");
   const [loading, setLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState("");
   const [weatherInfo, setWeatherInfo] = useState("");
@@ -17,37 +17,37 @@ const App = () => {
 
   useEffect(() => {
     const fetchWeatherData = async () => {
-      if (!searchCity) return;
       setLoading(true);
       try {
         const url = `${api.base}weather?q=${searchCity}&units=metric&APPID=${api.key}`;
         console.log("url", url);
         const response = await fetch(url);
-        const data = response.json();
+        const data = await response.json();
+        console.log("data", data);
         if (response.ok) {
-          setWeatherInfo(JSON.stringify(data));
+          setWeatherInfo(data);
           setErrorMessage("");
         } else {
           setErrorMessage(data.message);
         }
       } catch (error) {
+        console.log("error", error);
         setErrorMessage(error.message);
       }
       setLoading(false);
-      console.log("searching");
     };
     fetchWeatherData();
   }, [searchCity]);
   return (
     <>
-      <form
-        onSubmit={handleSubmit}
-        type="text"
-        action=""
-        placeholder="City"
-        value={searchInput}
-        onChange={(e) => setSearchInput(e.target.value)}
-      ></form>
+      <form onSubmit={handleSubmit} type="text" action="">
+        <input
+          type="text"
+          placeholder="City"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </form>
       {loading ? (
         <div>Loading...</div>
       ) : (
@@ -55,7 +55,16 @@ const App = () => {
           {errorMessage ? (
             <div style={{ color: "red" }}>{errorMessage}</div>
           ) : (
-            <div>{weatherInfo}</div>
+            <div>
+              Location: {weatherInfo.name}, {weatherInfo.sys.country}
+              <ul>
+                <li>Feels like: {weatherInfo.main.feels_like}</li>
+                <li>Humidity: {weatherInfo.main.humidity}</li>
+                <li>Temperature: {weatherInfo.main.temp}</li>
+                <li>Temp-max: {weatherInfo.main.temp_max}</li>
+                <li>Temp-min: {weatherInfo.main.temp_min}</li>
+              </ul>
+            </div>
           )}
         </>
       )}
